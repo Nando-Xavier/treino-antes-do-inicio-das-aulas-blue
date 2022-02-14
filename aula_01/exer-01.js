4d1813cf322f1cbdd4f6f0f2cdd2ce6a568dcaf0
Fernando Pereira Xavier <fernandoxavier233@gmail.com>
2022-02-14 20:24:04 -0300

    conclusão do exercício do primeiro módulo

--- aula_01/exer-01.js
+++ aula_01/exer-01.js
@@ -1,3 +1,8 @@
 const prompt = require('prompt-sync')();
-
+console.log();
 console.log('Hellow Wolrd');
+
+const A = prompt('Digite a nota A: ');
+const B = prompt('Digite a nota B: ');
+const media_ponderada = (A * 4 + B * 6) / 10;
+console.log(`A média ponderada do aluno é: ${media_ponderada}`);
