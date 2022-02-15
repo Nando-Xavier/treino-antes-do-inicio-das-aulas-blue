3439c98ca125ee126527f54a70215fa423a67590
Fernando Pereira Xavier <fernandoxavier233@gmail.com>
2022-02-14 21:28:56 -0300

    adicionado exercicio 2 do modulo 01

---
+++ aula_01/exer-02.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+const nome_peca_01 = prompt('Digite o nome da peça 01: ');
+const qtd_peca_01 = +prompt('Digite a quantidade de peça 01: ');
+const preco_peca_01 = +prompt('Digite o preço da peça 01: ');
+console.log();
+const nome_peca_02 = prompt('Digite o nome da peça 02: ');
+const qtd_peca_02 = +prompt('Digite a quantidade de peça 02: ');
+const preco_peca_02 = +prompt('Digite o preço da peça 02: ');
+console.log();
+const total01 = qtd_peca_01 * preco_peca_01;
+const total02 = qtd_peca_02 * preco_peca_02;
+console.log();
+console.log(`O valor a ser pago será de ${total01 + total02} reis.`);
