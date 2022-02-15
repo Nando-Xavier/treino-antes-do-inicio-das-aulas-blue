50bbe79c13eec7f7dae3129e74d7ec51686a2d3d
Fernando Pereira Xavier <fernandoxavier233@gmail.com>
2022-02-15 08:30:52 -0300

    adicinado terceiro exercicios

---
+++ aula_01/exer-03.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+console.log('Hellow Wolrd');
+const anos = prompt('Digite a sua idade em anos ');
+const meses = prompt('Digite quantos meses ');
+const dias = prompt('Digite quantos dias ');
+const qtd_diasAnos = anos * 365;
+const qtd_diasMeses = meses * 30;
+
+console.log(
+    `A quantidade de dias é: ${qtd_diasAnos + qtd_diasMeses + dias} dias.`,
+);
